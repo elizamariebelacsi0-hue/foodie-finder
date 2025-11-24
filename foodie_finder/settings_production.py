d9c142652f4801b7f45de43a4c8ced667f798aa0
Eliza Marie Rosario <elizamariebelacsi0@gmail.com>
2025-11-24 20:20:33 +0800

    Fix production settings - ALLOWED_HOSTS and CSRF

--- foodie_finder/settings_production.py
+++ foodie_finder/settings_production.py
@@ -8,7 +8,8 @@ DEBUG = False
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-1*iux(8r9%qq&rguv^j0k5x9)n=-9v5d#95p#r%$y9j_q%b0+5')
 
 ALLOWED_HOSTS = [
-    '.render.com',
+    'foodie-finder-or2v.onrender.com',
+    '.onrender.com',
     'localhost',
     '127.0.0.1',
 ]
@@ -28,17 +29,18 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Security settings
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
 
 # CSRF settings
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_SAMESITE = 'Lax'
+CSRF_TRUSTED_ORIGINS = [
+    'https://foodie-finder-or2v.onrender.com',
+    'https://*.onrender.com',
+]
